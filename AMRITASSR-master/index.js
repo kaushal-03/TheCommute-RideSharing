88fff5f67122773a6c01ac845b35196b8cc24bf4
kaushal_03 <ksiriguppa@gmail.com>
2023-01-16 14:54:35 +0530

    Initial Commits

---
+++ AMRITASSR-master/index.js
@@ -0,0 +1,90 @@
+const express = require("express");
+var app = express();
+app.set('view engine','ejs');
+var sources = [];
+var destinations=[];
+var dates=[]
+var bodyparser = require('body-parser');
+var mysql = require('mysql');
+const fs = require('fs');
+const { response } = require("express");
+const port = process.env.PORT || 3306;
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:true}))
+app.use(express.static('public'));
+app.use(express.static('publice'));
+app.use(express.static('publicee'));
+app.use(express.static('ecss'));
+var conn=mysql.createConnection({
+    host:"thecommute.mysql.database.azure.com", 
+    user:"amritassr", 
+    password:"Kaushal@2003", 
+    database:"carstats", port:3306
+});
+
+conn.connect(function(err){
+  if(err)throw err;
+  console.log("connected succcessfulllyyyyy!!!!");
+})
+
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname+'/publicee/index.html')
+})
+app.post('/add.html',(req,res)=>{
+  var destination = req.body.destination;
+  var source = req.body.source;
+  var date = req.body.birthday;
+    var sql = "INSERT INTO rides (source,destination,journeydate) VALUES('"+source.toLowerCase()+"','"+destination.toLowerCase()+"','"+date+"')";
+    conn.query(sql,function(error,result){
+      if(error){
+        res.sendFile(__dirname+'/public/failure.html')
+        throw error;
+      } 
+      console.log("connected");
+      res.sendFile(__dirname+'/public/success.html');
+    })
+})
+app.post('/find.html',(req,res)=>{
+    var source = req.body.source;
+    var destination = req.body.destination;
+    var date = req.body.birthday;
+    //console.log(source)
+    //console.log(destination)
+    //console.log(date)
+    var sql = "SELECT * FROM rides WHERE source='"+source.toLowerCase()+"' AND destination='"+destination.toLowerCase()+"' AND journeydate='"+date+"'";
+    conn.query(sql,(error,results)=>{
+        if(error){
+            res.sendFile(__dirname+'/public/failure.html')
+            throw error;
+        }
+        //var response = ""
+        //var i = 1;
+        results.forEach(element => {
+            //response = response + "<h3>Trip - "+i+"</h3>source: "+element.source+"<br> destination: "+element.destination+"<br> journeydate: "+element.journeydate+"<br><br>"
+            //i= i+1
+            //res.render("index",{source:element.source,destination:element.destination,date:element.journeydate})
+            sources.push(element.source);
+            destinations.push(element.destination);
+            dates.push(element.journeydate);
+            
+        })
+        res.redirect("/out")
+        console.log(sources);
+        console.log(destinations);
+        console.log(dates);
+       
+        //res.send(response)
+    })
+})
+app.get('/out',(req,res)=>{
+  res.render("index",{sourcees:sources,destinatiions:destinations,datees:dates})
+  sources=[];
+  destinations=[];
+  dates=[];
+})
+
+
+app.listen(port,()=>{
+    console.log("server started at "+port);
+});
